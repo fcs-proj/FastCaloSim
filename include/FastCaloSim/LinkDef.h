67fa3a7cd44b10b687c54db34118b542a78d0f9f
jbeirer <39070576+jbeirer@users.noreply.github.com>
2024-04-08 20:12:02 +0200

    Add basic cmake and CI setup and TFCSParametrization base class (#3)

---
+++ include/FastCaloSim/LinkDef.h
@@ -0,0 +1,17 @@
+#include "FastCaloSim/MLogging.h"
+#include "FastCaloSim/TFCSParametrizationBase.h"
+
+#ifdef __CLING__
+// Standard preamble: turn off creation of dictionaries for "everything":
+// we then turn it on only for the types we are interested in.
+#  pragma link off all globals;
+#  pragma link off all classes;
+#  pragma link off all functions;
+// Turn on creation of dictionaries for nested classes
+#  pragma link C++ nestedclasses;
+
+// Custom classes
+#  pragma link C++ class ISF_FCS::MLogging + ;
+#  pragma link C++ class TFCSParametrizationBase + ;
+
+#endif  // __CLING__
