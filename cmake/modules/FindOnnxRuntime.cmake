c1598db78c728b700f1ffa89e0cd25ba31ddfd2f
Joshua Falco Beirer <joshua.falco.beirer@cern.ch>
2024-04-16 15:11:25 +0200

    Improve ONNX CMake find config

--- cmake/modules/FindOnnxRuntime.cmake
+++ cmake/modules/FindOnnxRuntime.cmake
@@ -1,20 +1,53 @@
-# Find the ONNX Runtime include directory and library.
-#
-# This module defines the `onnxruntime` imported target that encodes all
-# necessary information in its target properties.
+include(FeatureSummary)
+set_package_properties(
+    ONNXRuntime PROPERTIES
+    URL "https://onnxruntime.ai/"
+    DESCRIPTION "Machine learning runtime")
 
+set(ONNXRuntime_ROOT_DIR
+    "${ONNXRuntime_ROOT_DIR}"
+    CACHE PATH "Root to search for ONNXRuntime")
 
-find_library(OnnxRuntime_LIBRARY onnxruntime HINTS /usr/local/lib64/)
-  
-find_path(OnnxRuntime_INCLUDE_DIR onnxruntime_cxx_api.h HINTS /usr/local/include/onnxruntime)
+find_package(PkgConfig)
+pkg_check_modules(PC_ONNXRuntime QUIET libonnxruntime)
+
+find_library(
+    ONNXRuntime_LIBRARY
+    NAMES onnxruntime
+    PATHS ${ONNXRuntime_ROOT_DIR}
+    PATH_SUFFIXES lib lib32 lib64
+    HINTS ${PC_ONNXRuntime_LIBRARY_DIRS})
+
+find_path(
+    ONNXRuntime_INCLUDE_DIR onnxruntime_cxx_api.h
+    NAMES core/session/onnxruntime_cxx_api.h onnxruntime_cxx_api.h
+    REQUIRED
+    PATHS ${ONNXRuntime_ROOT_DIR}
+    PATH_SUFFIXES onnxruntime include include/onnxruntime onnxruntime/core/session 
+                  include/onnxruntime/core/session
+    HINTS ${PC_ONNXRuntime_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  OnnxRuntime
-  REQUIRED_VARS OnnxRuntime_LIBRARY OnnxRuntime_INCLUDE_DIR)
+    ONNXRuntime REQUIRED_VARS ONNXRuntime_INCLUDE_DIR ONNXRuntime_LIBRARY)
 
-add_library(OnnxRuntime SHARED IMPORTED)
-set_property(TARGET OnnxRuntime PROPERTY IMPORTED_LOCATION ${OnnxRuntime_LIBRARY})
-set_property(TARGET OnnxRuntime PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OnnxRuntime_INCLUDE_DIR})
+if(ONNXRuntime_FOUND)
+    set(ONNXRuntime_INCLUDE_DIRS ${ONNXRuntime_INCLUDE_DIR})
+    if(EXISTS "${ONNXRuntime_INCLUDE_DIR}/core/session")
+        set(ONNXRuntime_INCLUDE_DIRS ${ONNXRuntime_INCLUDE_DIRS} ${ONNXRuntime_INCLUDE_DIR}/core/session)
+    endif()
+    set(ONNXRuntime_LIBRARIES "${ONNXRuntime_LIBRARY}")
+    if(NOT TARGET ONNXRuntime::ONNXRuntime)
+        add_library(ONNXRuntime::ONNXRuntime UNKNOWN IMPORTED)
+    endif()
+    set_target_properties(
+        ONNXRuntime::ONNXRuntime PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                                            "${ONNXRuntime_INCLUDE_DIRS}")
+    set_target_properties(
+        ONNXRuntime::ONNXRuntime
+        PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                   IMPORTED_LOCATION "${ONNXRuntime_LIBRARY}")
+    mark_as_advanced(ONNXRuntime_INCLUDE_DIRS ONNXRuntime_LIBRARY)
+endif()
 
-mark_as_advanced(OnnxRuntime_FOUND OnnxRuntime_INCLUDE_DIR OnnxRuntime_LIBRARY)
+mark_as_advanced(ONNXRuntime_ROOT_DIR)
