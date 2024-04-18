282a824e7e1101df7e307363613c615e974e59f6
Joshua Falco Beirer <joshua.falco.beirer@cern.ch>
2024-04-18 17:51:49 +0200

    clang-format tests

--- test/source/basic_class_tests.cxx
+++ test/source/basic_class_tests.cxx
@@ -1,11 +1,11 @@
 #include <string>
+
 #include <gtest/gtest.h>
 
 #include "FastCaloSim/FastCaloSim.h"
 
-
-TEST(BasicClassTests, ClassExportOk) {
-  
+TEST(BasicClassTests, ClassExportOk)
+{
   auto const exported = exported_class {};
 
   auto const expected_name = std::string("FastCaloSim");
@@ -13,4 +13,3 @@ TEST(BasicClassTests, ClassExportOk) {
 
   EXPECT_EQ(expected_name, actual_name);
 }
-
