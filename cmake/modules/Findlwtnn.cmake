56c036357ffd92b8b97317d330f43e49d09c7037
Joshua Beirer <joshua.beirer@cern.ch>
2024-09-26 17:54:50 +0200

    Improve lwtnn config

--- cmake/modules/Findlwtnn.cmake
+++ cmake/modules/Findlwtnn.cmake
@@ -20,6 +20,15 @@ else()
    find_library(LWTNN_LIBRARY NAMES lwtnn)
 endif()
 
+# If the include directory or library wasn't found, try again with hints
+if(NOT LWTNN_INCLUDE_DIR)
+   find_path(LWTNN_INCLUDE_DIR NAMES "lwtnn" PATH_SUFFIXES include HINTS ${LWTNN_ROOT})
+endif()
+
+if(NOT LWTNN_LIBRARY)
+   find_library(LWTNN_LIBRARY NAMES lwtnn HINTS ${LWTNN_LIBRARY_DIR} ${LWTNN_ROOT}/lib)
+endif()
+
 # Set the include directories and libraries
 set(LWTNN_INCLUDE_DIRS ${LWTNN_INCLUDE_DIR})
 set(LWTNN_LIBRARIES ${LWTNN_LIBRARY})
@@ -27,4 +36,4 @@ set(LWTNN_LIBRARIES ${LWTNN_LIBRARY})
 # Handle the standard find_package arguments:
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(lwtnn DEFAULT_MSG LWTNN_INCLUDE_DIRS LWTNN_LIBRARIES)
-mark_as_advanced(LWTNN_FOUND LWTNN_INCLUDE_DIR LWTNN_INCLUDE_DIRS LWTNN_LIBRARIES)
+mark_as_advanced(LWTNN_FOUND LWTNN_INCLUDE_DIR LWTNN_INCLUDE_DIRS LWTNN_LIBRARIES LWTNN_LIBRARY_DIR)
