181b59651c0ad70084a89f9cac3b72ea8786afff
Joshua Beirer <joshua.beirer@cern.ch>
2024-10-31 14:54:09 +0100

    Modify cmake/silencer.cmake

--- cmake/silencer.cmake
+++ cmake/silencer.cmake
@@ -1,15 +1,30 @@
-# Function to deactivate checks for specified targets
 function(deactivate_checks)
   foreach(target IN LISTS ARGN)
     if (TARGET "${target}")
-      # Resolve the real target if it's an alias
+      # Resolve the actual target if it's an alias
       get_target_property(real_target "${target}" ALIASED_TARGET)
       if (NOT real_target)
         set(real_target "${target}")
       endif()
 
+      # Check if the target is IMPORTED
+      get_target_property(is_imported "${real_target}" IMPORTED)
+      if (is_imported)
+        continue()
+      endif()
+
+      # Get the target type
+      get_target_property(target_type "${real_target}" TYPE)
+
+      # Determine compile option scope
+      if (target_type STREQUAL "INTERFACE_LIBRARY")
+        set(compile_option_scope "INTERFACE")
+      else()
+        set(compile_option_scope "PRIVATE")
+      endif()
+
       # Deactivate compiler warnings
-      target_compile_options("${real_target}" PRIVATE "-w")
+      target_compile_options("${real_target}" ${compile_option_scope} "-w")
 
       # Deactivate clang-tidy and cppcheck
       set_target_properties("${real_target}" PROPERTIES
