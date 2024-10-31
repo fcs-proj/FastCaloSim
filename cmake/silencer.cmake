1342902686cf036f103ca84f70ea5f71e4ec67af
Joshua Beirer <joshua.beirer@cern.ch>
2024-10-31 13:37:00 +0100

    Attempt to use local fmt first

--- cmake/silencer.cmake
+++ cmake/silencer.cmake
@@ -1,16 +1,23 @@
-# Function to deactivate checks for a specified target
-function(deactivate_target_checks target)
-  set_target_properties(${target} PROPERTIES
-    COMPILE_FLAGS "-w" # Deactivate compiler warnings
-    CXX_CLANG_TIDY ""  # Deactivate clang-tidy
-    CXX_CPPCHECK ""    # Deactivate cppcheck
-  )
-endfunction()
-
-# Function that deactivates checks for a list of targets
-# E.g. deactivate_checks(target1 target2 target3)
+# Function to deactivate checks for specified targets
 function(deactivate_checks)
   foreach(target IN LISTS ARGN)
-    deactivate_target_checks(${target})
+    if (TARGET "${target}")
+      # Resolve the real target if it's an alias
+      get_target_property(real_target "${target}" ALIASED_TARGET)
+      if (NOT real_target)
+        set(real_target "${target}")
+      endif()
+
+      # Deactivate compiler warnings
+      target_compile_options("${real_target}" PRIVATE "-w")
+
+      # Deactivate clang-tidy and cppcheck
+      set_target_properties("${real_target}" PROPERTIES
+        CXX_CLANG_TIDY ""
+        CXX_CPPCHECK  ""
+      )
+    else()
+      message(WARNING "Target '${target}' not found.")
+    endif()
   endforeach()
 endfunction()
