456080145e45a389f0598316ad352d7c6a16d235
Joshua Beirer <39070576+jbeirer@users.noreply.github.com>
2025-02-25 16:20:01 +0100

    Add param skeleton (#58)

---
+++ cmake/executable.cmake
@@ -0,0 +1,26 @@
+# Adds an executable target with the given name and sources.
+# Can be used to add executables to the project.
+# Can be executed with: cmake --build . --target run_<NAME>
+
+function(add_exec NAME SOURCES)
+  add_executable("${NAME}" "executables/${NAME}.cxx" ${SOURCES})
+
+  target_include_directories("${NAME}" PRIVATE
+    ${CMAKE_CURRENT_SOURCE_DIR}/include
+  )
+
+  target_link_libraries("${NAME}" PRIVATE
+    FastCaloSim::FastCaloSim fmt::fmt-header-only
+  )
+
+  target_compile_features("${NAME}" PRIVATE cxx_std_17)
+
+  add_custom_target("run_${NAME}"
+    COMMAND "${NAME}"
+    VERBATIM
+  )
+
+  add_dependencies("run_${NAME}" "${NAME}")
+
+  deactivate_checks(${NAME})
+endfunction()
---
+++ param/executables/createParamSlice.cxx
@@ -0,0 +1,12 @@
+#include <fmt/core.h>
+
+// -- Core FastCaloSim includes
+#include "FastCaloSim/Core/TFCSParametrizationBase.h"
+
+// -- Your own param gen includes
+#include "FastCaloSimParam/YourClass.h"
+
+auto main() -> int {
+    fmt::print("Running Parametrization Slice Creation...\n");
+    return 0;
+}
