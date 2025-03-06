01701df11779f1457c8a0d05b0453725f30aad44
Joshua Beirer <joshua.beirer@cern.ch>
2025-03-06 15:11:09 +0100

    Create separate param shared lib

--- cmake/executable.cmake
+++ cmake/executable.cmake
@@ -2,15 +2,17 @@
 # Can be used to add executables to the project.
 # Can be executed with: cmake --build . --target run_<NAME>
 
-function(add_exec NAME SOURCES)
-  add_executable("${NAME}" "executables/${NAME}.cxx" ${SOURCES})
+function(add_exec NAME)
+  add_executable("${NAME}" "executables/${NAME}.cxx")
 
   target_include_directories("${NAME}" PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/include
   )
 
   target_link_libraries("${NAME}" PRIVATE
-    FastCaloSim::FastCaloSim fmt::fmt-header-only
+    FastCaloSim::FastCaloSim
+    FastCaloSimParam::FastCaloSimParam
+    fmt::fmt-header-only
   )
 
   target_compile_features("${NAME}" PRIVATE cxx_std_17)
