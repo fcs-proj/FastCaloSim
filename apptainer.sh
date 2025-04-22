0679d2877335dc2fe835f479032a66b88ee9327b
Joshua Beirer <joshua.beirer@cern.ch>
2025-04-22 16:07:33 +0200

    Fix apptainer command

--- apptainer.sh
+++ apptainer.sh
@@ -1,6 +1,7 @@
 apptainer exec --contain --cleanenv --pwd /FastCaloSim \
-  -B $(pwd):/FastCaloSim \
-  -B /tmp \
-  -B $HOME/.gitconfig:$HOME/.gitconfig \
-  docker://fcsproj/fastcalosim-alma9:main \
-  bash -c "export PRE_COMMIT_HOME=/tmp/pre-commit-cache && pre-commit install && bash"
+ -B $(pwd):/FastCaloSim \
+ -B /tmp \
+ -B $HOME/.gitconfig:$HOME/.gitconfig \
+ -B $HOME/.git-credentials:$HOME/.git-credentials \
+ docker://fcsproj/fastcalosim-alma9:main \
+ bash -c "export PRE_COMMIT_HOME=/tmp/pre-commit-cache && pre-commit install && bash"
