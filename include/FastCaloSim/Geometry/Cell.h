399d11d65a4337396098aea823ac03b3a09dc510
Joshua Beirer <39070576+jbeirer@users.noreply.github.com>
2025-05-02 14:56:02 +0200

    Make cell data trivially copiable (#77)

--- include/FastCaloSim/Geometry/Cell.h
+++ include/FastCaloSim/Geometry/Cell.h
@@ -3,6 +3,9 @@
 #pragma once
 
 #include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <ostream>
 
 #include <fmt/core.h>
 #include <fmt/format.h>
@@ -11,24 +14,22 @@ struct Position
 {
   double m_x, m_y, m_z;
   double m_eta, m_phi, m_r;
-  auto x() const -> double { return m_x; }
-  auto y() const -> double { return m_y; }
-  auto z() const -> double { return m_z; }
-  auto eta() const -> double { return m_eta; }
-  auto phi() const -> double { return m_phi; }
-  auto r() const -> double { return m_r; }
+  constexpr auto x() const -> double { return m_x; }
+  constexpr auto y() const -> double { return m_y; }
+  constexpr auto z() const -> double { return m_z; }
+  constexpr auto eta() const -> double { return m_eta; }
+  constexpr auto phi() const -> double { return m_phi; }
+  constexpr auto r() const -> double { return m_r; }
 };
 
-// Base class for cells
-class Cell
+struct CellData
 {
-private:
   /// @brief Unique identifier for the cell
-  unsigned long long m_id;
+  uint64_t m_id;
   /// @brief Position of the cell in 3D space (x, y, z, eta, phi, r)
   Position m_pos;
   /// @brief Layer of the cell
-  unsigned int m_layer;
+  uint32_t m_layer;
   /// @brief Flag to indicate if the cell is in the barrel
   bool m_isBarrel;
   /// @brief Flags to indicate the coordinate system of the (cuboid) cell
@@ -36,8 +37,14 @@ private:
   /// @brief Cell sizes
   double m_dx, m_dy, m_dz, m_deta, m_dphi, m_dr;
   /// @brief Flag to invalidate cell
-  bool m_is_valid = true;
+  bool m_is_valid;
+};
 
+static_assert(std::is_trivially_copyable<CellData>::value,
+              "CellData must be trivially copiable");
+
+class Cell
+{
 public:
   enum SubPos
   {
@@ -46,9 +53,22 @@ public:
     EXT = 2  // exit
   };
 
-  Cell(unsigned long long id,
+  Cell()
+  {
+    m_data.m_id = uint64_t(-1);
+    m_data.m_pos = {0, 0, 0, 0, 0, 0};
+    m_data.m_layer = uint32_t(-1);
+    m_data.m_isBarrel = false;
+    m_data.m_isXYZ = m_data.m_isEtaPhiR = m_data.m_isEtaPhiZ =
+        m_data.m_isRPhiZ = false;
+    m_data.m_dx = m_data.m_dy = m_data.m_dz = 0;
+    m_data.m_deta = m_data.m_dphi = m_data.m_dr = 0;
+    m_data.m_is_valid = false;
+  }
+
+  Cell(uint64_t id,
        Position pos,
-       unsigned int layer,
+       uint32_t layer,
        bool isBarrel,
        bool isXYZ,
        bool isEtaPhiR,
@@ -60,113 +80,68 @@ public:
        double deta,
        double dphi,
        double dr)
-
-      : m_id(id)
-      , m_pos(std::move(pos))
-      , m_layer(layer)
-      , m_isBarrel(isBarrel)
-      , m_isXYZ(isXYZ)
-      , m_isEtaPhiR(isEtaPhiR)
-      , m_isEtaPhiZ(isEtaPhiZ)
-      , m_isRPhiZ(isRPhiZ)
-      , m_dx(dx)
-      , m_dy(dy)
-      , m_dz(dz)
-      , m_deta(deta)
-      , m_dphi(dphi)
-      , m_dr(dr)
   {
+    m_data.m_id = id;
+    m_data.m_pos = pos;
+    m_data.m_layer = layer;
+    m_data.m_isBarrel = isBarrel;
+    m_data.m_isXYZ = isXYZ;
+    m_data.m_isEtaPhiR = isEtaPhiR;
+    m_data.m_isEtaPhiZ = isEtaPhiZ;
+    m_data.m_isRPhiZ = isRPhiZ;
+    m_data.m_dx = dx;
+    m_data.m_dy = dy;
+    m_data.m_dz = dz;
+    m_data.m_deta = deta;
+    m_data.m_dphi = dphi;
+    m_data.m_dr = dr;
+    m_data.m_is_valid = true;
   }
 
-  // Allow easy creation of an invalid cell
-  Cell()
-      : m_id(-1)
-      , m_pos(Position {0, 0, 0, 0, 0, 0})
-      , m_layer(-1)
-      , m_isBarrel(false)
-      , m_isXYZ(false)
-      , m_isEtaPhiR(false)
-      , m_isEtaPhiZ(false)
-      , m_isRPhiZ(false)
-      , m_dx(0)
-      , m_dy(0)
-      , m_dz(0)
-      , m_deta(0)
-      , m_dphi(0)
-      , m_dr(0)
-      , m_is_valid(false)
+  auto id() const -> uint64_t { return m_data.m_id; }
+  auto x() const -> double { return m_data.m_pos.m_x; }
+  auto y() const -> double { return m_data.m_pos.m_y; }
+  auto z() const -> double { return m_data.m_pos.m_z; }
+  auto eta() const -> double { return m_data.m_pos.m_eta; }
+  auto phi() const -> double { return m_data.m_pos.m_phi; }
+  auto r() const -> double { return m_data.m_pos.m_r; }
+  auto layer() const -> uint32_t { return m_data.m_layer; }
+  auto isBarrel() const -> bool { return m_data.m_isBarrel; }
+  auto isXYZ() const -> bool { return m_data.m_isXYZ; }
+  auto isEtaPhiR() const -> bool { return m_data.m_isEtaPhiR; }
+  auto isEtaPhiZ() const -> bool { return m_data.m_isEtaPhiZ; }
+  auto isRPhiZ() const -> bool { return m_data.m_isRPhiZ; }
+  auto dx() const -> double { return m_data.m_dx; }
+  auto dy() const -> double { return m_data.m_dy; }
+  auto dz() const -> double { return m_data.m_dz; }
+  auto deta() const -> double { return m_data.m_deta; }
+  auto dphi() const -> double { return m_data.m_dphi; }
+  auto dr() const -> double { return m_data.m_dr; }
+  auto is_valid() const -> bool { return m_data.m_is_valid; }
+  void invalidate() { m_data.m_is_valid = false; }
+  auto raw() const -> const CellData& { return m_data; }
+
+  auto rent() const -> double
   {
+    assert(m_data.m_dr > 0 && "rent() called on cell with dr <= 0. The half-width of the cell seems undefined.");
+    return r() - m_data.m_dr * 0.5;
+  }
+  auto rext() const -> double
+  {
+    assert(m_data.m_dr > 0 && "rext() called on cell with dr <= 0.  The half-width of the cell seems undefined.");
+    return r() + m_data.m_dr * 0.5;
+  }
+  auto zent() const -> double
+  {
+    assert(m_data.m_dz > 0 && "zent() called on cell with dz <= 0. The half-width of the cell seems undefined.");
+    return z() < 0 ? z() + m_data.m_dz * 0.5 : z() - m_data.m_dz * 0.5;
+  }
+  auto zext() const -> double
+  {
+    assert(m_data.m_dz > 0 && "zext() called on cell with dz <= 0.  The half-width of the cell seems undefined.");
+    return z() < 0 ? z() - m_data.m_dz * 0.5 : z() + m_data.m_dz * 0.5;
   }
 
-  virtual ~Cell() = default;
-
-  // Cell validity
-  auto inline is_valid() const -> bool { return m_is_valid; }
-  auto inline invalidate() -> void { m_is_valid = false; }
-
-  // Direct accessors
-  auto inline id() const -> unsigned long long { return m_id; }
-  auto inline x() const -> double { return m_pos.x(); }
-  auto inline y() const -> double { return m_pos.y(); }
-  auto inline z() const -> double { return m_pos.z(); }
-  auto inline eta() const -> double { return m_pos.eta(); }
-  auto inline phi() const -> double { return m_pos.phi(); }
-  auto inline r() const -> double { return m_pos.r(); }
-  auto inline layer() const -> unsigned int { return m_layer; }
-  auto inline isBarrel() const -> bool { return m_isBarrel; }
-
-  auto inline isXYZ() const -> bool { return m_isXYZ; }
-  auto inline isEtaPhiR() const -> bool { return m_isEtaPhiR; }
-  auto inline isEtaPhiZ() const -> bool { return m_isEtaPhiZ; }
-  auto inline isRPhiZ() const -> bool { return m_isRPhiZ; }
-
-  /// @brief Cell size accessors
-  /// Note: For the hit->cell matching, only the cell sizes
-  /// for the coordinate system of the cell are used
-  /// (e.g. for x,y,z cells, only dx, dy, dz are needed)
-  /// However: for the extrapolation we currently also take into account
-  /// the half width of the cell in the other coordinate systems
-  /// e.g. for TileBar0 (eta,phi,r) we also use dz which is stored in the ROOT
-  /// file In essence: if you call e.g. zent() on any cell it will add whatever
-  /// dz is stored We might want to revisit the logic in the future
-  /// to prevent misuse for now, we assert that stored cell widths are non-zero
-  /// in the case e.g. zent() or zext() is called
-  auto inline dx() const -> double { return m_dx; }
-  auto inline dy() const -> double { return m_dy; }
-
-  auto inline dz() const -> double { return m_dz; }
-
-  auto inline deta() const -> double { return m_deta; }
-
-  auto inline dphi() const -> double { return m_dphi; }
-
-  auto inline dr() const -> double { return m_dr; }
-
-  auto inline rent() const -> double
-  {
-    assert(m_dr > 0 && "rent() called on cell with dr <= 0. The half-width of the cell seems undefined.");
-    return r() - m_dr * 0.5;
-  }
-
-  auto inline rext() const -> double
-  {
-    assert(m_dr > 0 && "rext() called on cell with dr <= 0.  The half-width of the cell seems undefined.");
-    return r() + m_dr * 0.5;
-  }
-
-  auto inline zent() const -> double
-  {
-    assert(m_dz > 0 && "zent() called on cell with dz <= 0. The half-width of the cell seems undefined.");
-    return z() < 0 ? z() + m_dz * 0.5 : z() - m_dz * 0.5;
-  }
-
-  auto inline zext() const -> double
-  {
-    assert(m_dz > 0 && "zext() called on cell with dz <= 0.  The half-width of the cell seems undefined.");
-    return z() < 0 ? z() - m_dz * 0.5 : z() + m_dz * 0.5;
-  }
-
-  // only makes ense for barrel
   auto inline r(SubPos subpos) const -> double
   {
     switch (subpos) {
@@ -210,28 +185,22 @@ public:
   /// - A positive value indicates that the hit is outside the cell, with the
   /// magnitude representing the distance from the cell boundary.
   ///
-  auto inline boundary_proximity(const Position& pos) const -> double
+  auto boundary_proximity(const Position& pos) const -> double
   {
-    if (m_isXYZ) {
-      double delta_x = std::abs(pos.x() - m_pos.x());
-      double delta_y = std::abs(pos.y() - m_pos.y());
-
-      return std::max(delta_x - m_dx / 2, delta_y - m_dy / 2);
+    if (m_data.m_isXYZ) {
+      double dx = std::abs(pos.m_x - x());
+      double dy = std::abs(pos.m_y - y());
+      return std::max(dx - m_data.m_dx / 2, dy - m_data.m_dy / 2);
     }
-
-    if (m_isEtaPhiR || m_isEtaPhiZ || m_isRPhiZ) {
-      // Note: if deta is not provided for RPhiZ cells,
-      // approximate deta values will be pre-computed at load time
-      double delta_eta = std::abs(pos.eta() - m_pos.eta());
-      double delta_phi = std::abs(norm_angle(pos.phi() - m_pos.phi()));
-      return std::max(delta_eta / m_deta, delta_phi / m_dphi) - 0.5;
+    if (m_data.m_isEtaPhiR || m_data.m_isEtaPhiZ || m_data.m_isRPhiZ) {
+      double d_eta = std::abs(pos.m_eta - eta());
+      double d_phi = std::abs(norm_angle(pos.m_phi - phi()));
+      return std::max(d_eta / m_data.m_deta, d_phi / m_data.m_dphi) - 0.5;
     }
-
-    std::runtime_error("Cell is not in a valid coordinate system");
-    return 0;
+    throw std::runtime_error("Invalid coordinate system");
   }
 
-  auto inline is_inside(const Position& pos) const -> bool
+  auto is_inside(const Position& pos) const -> bool
   {
     return boundary_proximity(pos) < 0;
   }
@@ -252,16 +221,16 @@ public:
         "  phi: {:10.2f}\n"
         "  eta: {:10.2f}\n"
         "  r: {:10.2f}\n",
-        cell.m_id,
-        cell.m_layer,
-        cell.m_pos.x(),
-        cell.m_pos.y(),
-        cell.m_pos.z(),
-        cell.m_pos.phi(),
-        cell.m_pos.eta(),
-        cell.m_pos.r());
+        cell.id(),
+        cell.layer(),
+        cell.x(),
+        cell.y(),
+        cell.z(),
+        cell.phi(),
+        cell.eta(),
+        cell.r());
 
-    if (cell.m_isXYZ) {
+    if (cell.isXYZ()) {
       os << fmt::format(
           "Cell size:\n"
           "  dx: {:10.2f}\n"
@@ -272,8 +241,7 @@ public:
           cell.dy(),
           cell.dz());
     }
-
-    if (cell.m_isEtaPhiR) {
+    if (cell.isEtaPhiR()) {
       os << fmt::format(
           "Cell size:\n"
           "  dphi: {:10.2f}\n"
@@ -284,8 +252,7 @@ public:
           cell.deta(),
           cell.dr());
     }
-
-    if (cell.m_isEtaPhiZ) {
+    if (cell.isEtaPhiZ()) {
       os << fmt::format(
           "Cell size:\n"
           "  dphi: {:10.2f}\n"
@@ -296,8 +263,7 @@ public:
           cell.deta(),
           cell.dz());
     }
-
-    if (cell.m_isRPhiZ) {
+    if (cell.isRPhiZ()) {
       os << fmt::format(
           "Cell size:\n"
           "  dphi: {:10.2f}\n"
@@ -308,7 +274,9 @@ public:
           cell.dz(),
           cell.dr());
     }
-
     return os;
   }
+
+private:
+  CellData m_data;
 };
